c54d49e745f114fa36020538f85389ecfbef9384
theetha1608 <theerthakunjusss@gmail.com>
2024-04-05 20:07:14 +0530

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Reactive-Forms';
+  form !:FormGroup;
+  booklist: any[] = [];
+
+  constructor(private fb:FormBuilder)
+  {}
+    ngOnInit()
+    {
+    this.form=this.fb.group({
+      Book_Name:['', Validators.required],
+      Author:['', Validators.required],
+      Price:['', Validators.required],
+      Genre:['', Validators.required],
+      Published_Date:['', Validators.required],
+      Country:['', Validators.required],
+      Description:['', Validators.required],
+    })
+    }
+    submit()
+    {
+      this.booklist.push(this.form.value);     
+      console.log(this.booklist);
+     
+    }
+  
 }
+
